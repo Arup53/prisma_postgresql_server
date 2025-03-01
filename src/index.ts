62dc06e11e9a5f41b11db1fcc42351ad4b43468e
Arup53 <arupkumarhowlader12@gmail.com>
2025-03-01 10:57:54 +0600

    add checkpoint

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import axios from "axios";
+import { PrismaClient } from "@prisma/client";
 
 dotenv.config();
 
 const app: Express = express();
+const prisma = new PrismaClient();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
@@ -13,10 +15,6 @@ app.use(cors());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Ulla ulla jinga laga");
-});
-
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
 });
 
 let coinData;
@@ -49,9 +47,16 @@ const fetchAndCacheCoins = async () => {
     console.log(allCoins);
 
     console.log("Coin data cached successfully!");
+    return allCoins;
   } catch (error) {
     console.error("Error fetching CoinGecko data:", error);
   }
 };
 
-setInterval(fetchAndCacheCoins, 120000);
+fetchAndCacheCoins();
+
+// setInterval(fetchAndCacheCoins, 120000);
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
