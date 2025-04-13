71eb40fec3a262c1a8d4db38de1bb1eda2486ba5
Arup53 <arupkumarhowlader12@gmail.com>
2025-04-13 11:36:53 +0600

    implement user and card creation api endpoint

---
+++ src/cardTranscation.ts
@@ -0,0 +1,40 @@
+import dotenv from "dotenv";
+import Stripe from "stripe";
+
+dotenv.config();
+const stripe = new Stripe(process.env.STRIPE_TEST_API_KEY || "");
+
+export async function test() {
+  const cardholder = await stripe.issuing.cardholders.create({
+    name: "carl Rosen",
+    email: "carl.rosen@example.com",
+    phone_number: "+18008675309",
+    status: "active",
+    type: "individual",
+    individual: {
+      first_name: "carl",
+      last_name: "Rosen",
+      dob: { day: 1, month: 11, year: 1981 },
+    },
+    billing: {
+      address: {
+        line1: "123 Main Street",
+        city: "San Francisco",
+        state: "CA",
+        postal_code: "94111",
+        country: "US",
+      },
+    },
+  });
+
+  const card = await stripe.issuing.cards.create({
+    cardholder: cardholder.id,
+    currency: "usd",
+    type: "virtual",
+  });
+
+  return {
+    cardholderId: cardholder.id,
+    cardId: card.id,
+  };
+}
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import axios from "axios";
 import { PrismaClient } from "@prisma/client";
+import { test } from "./cardTranscation.js";
 
 dotenv.config();
 
@@ -20,6 +21,39 @@ app.get("/", (req: Request, res: Response) => {
 // const res = await getUser("koi");
 
 // console.log(res);
+
+app.post("/users", async (req, res) => {
+  const { name } = req.body;
+  try {
+    const user = await prisma.user.create({
+      data: { name },
+      select: {
+        id: true,
+      },
+    });
+    const cardObj = await test();
+
+    const response = await prisma.cardDetails.create({
+      data: {
+        id: user.id,
+        cardholder_Id: cardObj.cardholderId,
+        card_id: cardObj.cardId,
+      },
+      select: {
+        cardholder_Id: true,
+        card_id: true,
+      },
+    });
+
+    res.json({
+      message: "success",
+      cardHolderId: response.cardholder_Id,
+      cardId: response.card_id,
+    });
+  } catch (err) {
+    res.status(500).json({ error: "Error creating user" });
+  }
+});
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
